51f9905227e73497f271ed06f8380d4b65fb8f2b
spammy <spammy7147@gamil.com>
2023-03-20 23:26:35 +0900

    first commit

---
+++ src/main/java/gg/rebootduo/reboot/RebootApplication.java
@@ -0,0 +1,13 @@
+package gg.rebootduo.reboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gg/rebootduo/reboot/RebootApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.rebootduo.reboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
